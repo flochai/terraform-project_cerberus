8921b5257bb66d4daef3999641c299e36d3fd3c7
Florian Chaillou <tighter-bandage-5j@icloud.com>
2025-09-23 09:37:48 +0200

    fix: update variable name

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -1,17 +1,17 @@
 output "address" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.chimera_db.address
+  value       = aws_db_instance.cerberus_db.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.chimera_db.port
+  value       = aws_db_instance.cerberus_db.port
   sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.chimera_db.username
+  value       = aws_db_instance.cerberus_db.username
   sensitive   = true
 }
