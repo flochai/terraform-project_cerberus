86eb16509512716f263d123976aedcdece1e73b4
Florian Chaillou <tighter-bandage-5j@icloud.com>
2025-09-23 09:36:12 +0200

    fix: update variable name

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,13 +1,13 @@
 output "private_ip" {
-  value = aws_instance.chimera-instance.private_ip
+  value = aws_instance.cerberus-instance.private_ip
 }
 
 output "instance_id" {
   description = "ID of the public EC2 instance"
-  value       = aws_instance.chimera-instance.id
+  value       = aws_instance.cerberus-instance.id
 }
 
 output "public_instance_az" {
   description = "AZ of the public EC2 instance"
-  value       = aws_instance.chimera-instance.availability_zone
+  value       = aws_instance.cerberus-instance.availability_zone
 }
