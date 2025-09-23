8234230002abfed69052c46c1ca70537ff05724e
Florian Chaillou <tighter-bandage-5j@icloud.com>
2025-09-23 09:37:20 +0200

    fix: update variable name

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,10 +1,10 @@
-resource "aws_db_instance" "chimera_db" {
-  identifier             = "chimera-db"
+resource "aws_db_instance" "cerberus_db" {
+  identifier             = "cerberus-db"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
   engine                 = "mariadb"
   engine_version         = "10.11"
-  username               = "chimera1"
+  username               = "cerberus1"
   password               = var.db_password
   db_subnet_group_name   = var.db_subnet_group_name
   vpc_security_group_ids = [var.sg_priv_id]
